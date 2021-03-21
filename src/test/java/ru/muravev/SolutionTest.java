25b1f337a6b87fd259cab738e11137369be5d5fd
Муравьев Артём Валерьевич <Artem.Muravev@rosbank.ru>
2021-03-21 14:08:28 +0300

    Basic app for primary testing

---
+++ src/test/java/ru/muravev/SolutionTest.java
@@ -0,0 +1,16 @@
+package ru.muravev;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+
+    @Test
+    public void multipleInt() {
+        Assert.assertEquals(5, solution.multipleInt(1,4));
+    }
+}
